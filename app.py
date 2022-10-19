516a27c938ccd753d939925e1372ee2e2f516136
Thi-Tracey_Nguyen <traceynguyen.1988@gmail.com>
2022-10-19 10:10:52 +0700

    Authorisation added

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
-from datetime import date
-from flask_marshmallow import Marshmallow
+from sqlalchemy.exc import IntegrityError
+from datetime import date, timedelta
+from flask_marshmallow import Marshmallow, fields
 from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 
 
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://trello_dev:password123@127.0.0.1:5432/trello'
+app.config['JWT_SECRET_KEY'] = 'hello there'
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 bcrypt = Bcrypt(app)
-
-
+jwt = JWTManager(app)
 
 class Card(db.Model):
     __tablename__ = 'cards'
@@ -37,15 +38,20 @@ class User(db.Model):
 
 class UserSchema(ma.Schema):
     class Meta:
-        fields = ('id', 'name', 'email', 'is_admin')
-
-
-
+        fields = ('id', 'name', 'email', 'password', 'is_admin')
+        
 
 class CardSchema(ma.Schema):
     class Meta:
         fields = ("id", "title", "description", "date", "status", "priority")
         ordered = True
+
+def authorize():
+    user_id = get_jwt_identity() #extract the user identity from the token
+    stmt = db.select(User).filter_by(id = user_id)
+    user = db.session.scalar(stmt)
+    return user.is_admin
+    
 
 @app.cli.command('create')
 def create_db():
@@ -108,14 +114,47 @@ def drop_tables():
     print('Tables dropped')
 
 
+@app.route('/auth/register/', methods=['POST'])
+def auth_register():
+    try:
+        user = User(
+            email = request.json['email'],
+            password = bcrypt.generate_password_hash(request.json['password']).decode('utf-8'),
+            name = request.json['name']
+        )
+        db.session.add(user)
+        db.session.commit()
+        #Respond to the user
+        return UserSchema(exclude=['password']).dump(user), 201
+    except IntegrityError:
+        return {'error': 'Email address already in use'}, 409
+
+
 @app.route('/cards/')
+@jwt_required()
 def all_cards():
+    if not authorize():
+        return {"error": "You must be an admin"}, 401
     # cards = Card.query.all()
     # return CardSchema(many=True).dump(cards)
     stmt = db.select(Card).order_by(Card.priority, Card.title)
     cards = db.session.scalars(stmt)
     return CardSchema(many=True).dump(cards)
   
+
+@app.route('/auth/login/', methods=['POST'])
+def auth_login():
+    stmt = db.select(User).filter_by(email = request.json["email"])
+    user = db.session.scalar(stmt)
+    if user and bcrypt.check_password_hash(user.password, request.json["password"]):
+        # return UserSchema(exclude=["password"]).dump(user), 200
+        # generate token
+        token = create_access_token(identity=str(user.id), expires_delta=timedelta(days=1))
+        return {'email': user.email, 'token': token, 'is_admin': user.is_admin}
+    else:
+        return {"error": "Invalid email or password"}, 401 #401 Unauthorized
+
+
 @app.cli.command('first_card')
 def first_card():
     # card = Card.query.first()
